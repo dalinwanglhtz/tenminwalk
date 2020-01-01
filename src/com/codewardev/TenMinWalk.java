4cf1fcd83980a50e877e4fde86bbdfe14f08f90a
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-01 21:24:59 +1100

    started

---
+++ src/com/codewardev/TenMinWalk.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/54da539698b8a2ad76000228/train/java
+
+public class TenMinWalk {
+
+	public static boolean isValid(char[] walk) {
+		
+		return false;
+	}
+
+}
