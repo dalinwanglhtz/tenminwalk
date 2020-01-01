91b6dbdc30e0feaafc1afbf651efb32e5dce3e20
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-01 22:03:24 +1100

    first working cut

--- src/com/codewardev/TenMinWalk.java
+++ src/com/codewardev/TenMinWalk.java
@@ -1,12 +1,33 @@
 package com.codewardev;
 
+import java.util.Stack;
+
 // https://www.codewars.com/kata/54da539698b8a2ad76000228/train/java
 
 public class TenMinWalk {
 
 	public static boolean isValid(char[] walk) {
+		if(walk.length != 10) return false;
 		
-		return false;
+		Stack<Character> stack = new Stack<Character>();
+		for(int i=0; i<walk.length; i++) {
+			if(walk[i] == 'n' && stack.indexOf('s') != -1) {
+				stack.remove(stack.indexOf('s'));
+			} else
+			if(walk[i] == 's' && stack.indexOf('n') != -1) {
+				stack.remove(stack.indexOf('n'));
+			} else
+			if(walk[i] == 'w' && stack.indexOf('e') != -1) {
+				stack.remove(stack.indexOf('e'));
+			} else
+			if (walk[i] == 'e' && stack.indexOf('w') != -1) {
+				stack.remove(stack.indexOf('w'));
+			} else {
+				stack.push(walk[i]);
+			}
+		}
+		
+		return stack.size() == 0 ? true:false;
 	}
 
 }
